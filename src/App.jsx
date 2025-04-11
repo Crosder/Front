538921f431258579ecb854f2461bc301d40a9b22
Crosder <eahpromusico4@gmail.com>
2025-04-11 00:25:21 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import LoginPage from './pages/LoginPage';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-black text-white">
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
